0089dc8dcafd6d4b407d42ce452c132ce02b5347
Songyan Zhu <Songyan.Zhu@outlook.com>
2026-02-21 09:33:49 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: Songyan Zhu
+# Mail: songyan.zhu@soton.ac.uk
+# Created Time:  2018-10-23 13:28:34
+#############################################
+
+
+from setuptools import setup, find_packages
+
+setup(
+	name = "gges",
+	version = "0.0.1",
+	keywords = ("geography", "env science"),
+	description = "Geography and Environmental Science",
+	long_description = "Read/write and process rs/gis related data, especially atmospheric rs data.",
+	license = "MIT Licence",
+
+	url="https://github.com/songyanzhu/gges",
+	author = "Songyan Zhu",
+	author_email = "Songyan.Zhu@soton.ac.uk",
+
+	packages = find_packages(),
+	include_package_data = True,
+	platforms = "any",
+	install_requires=[
+            # "geopandas==0.4.1",
+            # "scipy==1.2.1",
+            # "GDAL==2.3.3",
+            # "pyproj==1.9.6",
+            # "Shapely==1.6.4.post1",
+            # "rasterio==1.0.21",
+            # "numpy==1.16.0",
+			# "geopandas",
+			# "rasterio"
+			# "pygal",
+			"geetools"
+
+	]
+)
